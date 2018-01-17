a434c4e950b383098d32ff6679822502abcfb912
Andrii Novitskyi <anovitskyi@Gispartner.pl>
2018-01-17 21:53:06 +0100

    Init commit

---
+++ app/src/main/java/com/anovitskyi/myfavouritetoys/MainActivity.java
@@ -0,0 +1,13 @@
+package com.anovitskyi.myfavouritetoys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
